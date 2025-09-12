577fc8cd00b80f414e906a1c6b6f22f29a57b368
ro208 <ricky.ngo@mail.utoronto.ca>
2025-09-12 13:32:25 -0400

    Completed task 4.1

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                j++;
+            }
+        }
+        return j;
+    }
+}
