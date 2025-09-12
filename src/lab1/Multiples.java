6d5fd6621a445308c2596e9b80af4a77f1abd507
ro208 <ricky.ngo@mail.utoronto.ca>
2025-09-12 14:29:29 -0400

    Fixing Markus

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,7 +5,7 @@ public class Multiples {
         System.out.println(multiples());
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int j = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
@@ -15,7 +15,7 @@ public class Multiples {
         return j;
     }
 
-    static int multiples() {
+    public static int multiples() {
         int n = 1000;
         int a = 3;
         int b = 5;
